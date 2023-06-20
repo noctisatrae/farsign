8ec825630f76607e7c2ed1bd16d3aef987ba157d
noctisatrae <pro.hector.kub@gmail.com>
2023-06-20 10:40:55 +0200

    identation -> space

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -2,56 +2,56 @@ import * as ed from "@noble/ed25519"
 import { bytesToHexString } from "@farcaster/hub-web";
 
 type keyGeneration = {
-    publicKey: Uint8Array,
-    privateKey: Uint8Array
+  publicKey: Uint8Array,
+  privateKey: Uint8Array
 }
 
 type weirdResult = {
-    token: string,
-    deepLinkUrl: string
+  token: string,
+  deepLinkUrl: string
 }
 
 type signerRequestResult = {
-    fid: string,
-    base64SignedMessage: string
+  fid: string,
+  base64SignedMessage: string
 }
 
 const generateKeyPair = async (): Promise<keyGeneration> => {
-    const privateKey = ed.utils.randomPrivateKey();
-    const publicKey = await ed.getPublicKeyAsync(privateKey);
-    return { publicKey, privateKey };
+  const privateKey = ed.utils.randomPrivateKey();
+  const publicKey = await ed.getPublicKeyAsync(privateKey);
+  return { publicKey, privateKey };
 }
 
 // extract key from keygen
 const sendPublicKey = async (publicKey: Uint8Array, name: string): Promise<weirdResult> => {
     
-    let convertedKey = bytesToHexString(publicKey)._unsafeUnwrap();
+  let convertedKey = bytesToHexString(publicKey)._unsafeUnwrap();
     
-    let response = await fetch("https://api.warpcast.com/v2/signer-requests", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ publicKey: convertedKey, name: name }),
-    });
+  let response = await fetch("https://api.warpcast.com/v2/signer-requests", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ publicKey: convertedKey, name: name }),
+  });
 
-    let {deepLinkUrl, token}: weirdResult = (await response.json()).result;
+  let {deepLinkUrl, token}: weirdResult = (await response.json()).result;
 
-    return { deepLinkUrl, token };
+  return { deepLinkUrl, token };
 }
 
 const requestSignerAuthStatus = async (token: string) => {
-    return await (
-            await fetch(`https://api.warpcast.com/v2/signer-request?token=${token}`, {
-                headers: {
-                "Content-Type": "application/json"
-                }
-            })
-    ).json();
+  return await (
+    await fetch(`https://api.warpcast.com/v2/signer-request?token=${token}`, {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+  ).json();
 }
 
 export {
-    generateKeyPair,
-    sendPublicKey,
-    requestSignerAuthStatus
+  generateKeyPair,
+  sendPublicKey,
+  requestSignerAuthStatus
 }
