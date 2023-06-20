b46c3b66c2faf5b65548d721a4f2c20b46efdc3e
noctisatrae <pro.hector.kub@gmail.com>
2023-06-20 11:20:22 +0200

    Add tasks for my IDE, and improve hooks

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,26 +1,50 @@
 import { useEffect, useState } from "react";
 import { generateKeyPair, requestSignerAuthStatus, sendPublicKey } from "@farsign/utils";
 
+type Token = {
+  token: string,
+  deepLink: string
+}
+
+type SignerData = {
+  token: string,
+  publicKey: string,
+  timestamp: number,
+  name: string,
+  fid: number,
+  messageHash: string,
+  base64SignedMessage: string
+}
+
+type Signer = {
+  signer:SignerData,
+  isConnected: boolean
+}
+
 const useToken = (clientName: string) => {
 
-  const [fetchedToken, setFetchedToken] = useState({
+  const [fetchedToken, setFetchedToken] = useState<Token>({
     token: "",
     deepLink: ""
   });
 
-  (async () => {
-    const { publicKey } = await generateKeyPair();
-    const {token, deepLinkUrl} = await sendPublicKey(publicKey, clientName);
-    
-    setFetchedToken({ token: token, deepLink: deepLinkUrl });
-  })();
+  console.log(fetchedToken);
+
+  useEffect(() => {
+    (async () => {
+      const { publicKey } = await generateKeyPair();
+      const {token, deepLinkUrl} = await sendPublicKey(publicKey, clientName);
+      
+      setFetchedToken({ token: token, deepLink: deepLinkUrl });
+    })();
+  }, []);
 
   return [fetchedToken, setFetchedToken]
 }
 
 const useSigner = (token: string) => {
 
-  const [signer, setSigner] = useState({
+  const [signer, setSigner] = useState<Signer>({
      signer: {
       token: "",
       publicKey: "",
@@ -33,25 +57,28 @@ const useSigner = (token: string) => {
      isConnected: false
   });
 
-  (async () => {
-    if (token.length > 0) {
-      while (true) {
-        await new Promise(resolve => setTimeout(resolve, 3000));
-
-        const data = await requestSignerAuthStatus(token);
-
-        if (data.result && data.result.signerRequest.base64SignedMessage) {
-          setSigner({
-            signer: data.result.signerRequest,
-            isConnected: true
-          });
-          break
+  useEffect(() => {
+    (async () => {
+      if (token.length > 0) {
+        while (true) {
+          await new Promise(resolve => setTimeout(resolve, 3000));
+  
+          const data = await requestSignerAuthStatus(token);
+  
+          if (data.result && data.result.signerRequest.base64SignedMessage) {
+            setSigner({
+              signer: data.result.signerRequest,
+              isConnected: true
+            });
+            break
+          }
         }
       }
-    }
-  })()
+    })()
+  }, [token])
 
   return [signer, setSigner]
 }
 
 export { useSigner, useToken };
+export type { Token, Signer, SignerData };
