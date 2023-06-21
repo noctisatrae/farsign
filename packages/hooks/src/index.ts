c3c6d745c9fa5f6835aee0064f3233d7e3239b46
noctisatrae <pro.hector.kub@gmail.com>
2023-06-21 11:38:49 +0200

    session storage system added :D

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -21,8 +21,22 @@ type Signer = {
   isConnected: boolean
 }
 
-const useToken = (clientName: string) => {
+const useCheckSigner = (clientName: string) => {
+  const [isConnected, setIsConnected] = useState(false);
 
+  useEffect(() => {
+    (async () => {
+      if (localStorage.getItem("farsign-base64SignedMessage" + clientName) === null) 
+        setIsConnected(false)
+      else 
+        setIsConnected(true) 
+    })();
+  }, [])
+
+  return [isConnected, setIsConnected];
+}
+
+const useToken = (clientName: string) => {
   const [fetchedToken, setFetchedToken] = useState<Token>({
     token: "",
     deepLink: ""
@@ -30,31 +44,16 @@ const useToken = (clientName: string) => {
 
   useEffect(() => {
     (async () => {
-      if (localStorage.getItem("privateKey-farsign") === null) {
         const { publicKey, privateKey } = await generateKeyPair();
         const {token, deepLinkUrl} = await sendPublicKey(publicKey, clientName);
-        
-        localStorage.setItem("privateKey-farsign", bytesToHexString(privateKey)._unsafeUnwrap());
-
-        setFetchedToken({ token: token, deepLink: deepLinkUrl });
-      } else {
-
-        const privateKey = localStorage.getItem("privateKey-farsign");
-        
-        // console.log("GETTING FROM LOCALSTORAGE " + privateKey)
-        
-        const publicKey = await getPublicKeyAsync(hexStringToBytes(privateKey!)._unsafeUnwrap());
-        
-        const {token, deepLinkUrl} = await sendPublicKey(publicKey, clientName);
-        setFetchedToken({ token: token, deepLink: deepLinkUrl });
-      }
+        setFetchedToken({ token: token, deepLink: deepLinkUrl })
     })();
   }, []);
 
   return [fetchedToken, setFetchedToken]
 }
 
-const useSigner = (token: string) => {
+const useSigner = (token: string, clientName: string) => {
 
   const [signer, setSigner] = useState<Signer>({
      signer: {
@@ -70,27 +69,34 @@ const useSigner = (token: string) => {
   });
 
   useEffect(() => {
-    (async () => {
-      if (token.length > 0) {
-        while (true) {
-          await new Promise(resolve => setTimeout(resolve, 3000));
+    if (localStorage.getItem("farsign-base64SignedMessage" + clientName) === null) {
+      (async () => {
+        if (token.length > 0) {
+          while (true) {
+            await new Promise(resolve => setTimeout(resolve, 3000));
+    
+            const data = await requestSignerAuthStatus(token);
+    
+            if (data.result && data.result.signerRequest.base64SignedMessage) {
   
-          const data = await requestSignerAuthStatus(token);
+              localStorage.setItem("farsign-base64SignedMessage" + clientName, data.result.signerRequest.base64SignedMessage);
   
-          if (data.result && data.result.signerRequest.base64SignedMessage) {
-            setSigner({
-              signer: data.result.signerRequest,
-              isConnected: true
-            });
-            break
+              setSigner({
+                signer: data.result.signerRequest,
+                isConnected: true
+              });
+              break
+            }
           }
         }
-      }
-    })()
+      })()
+    } else {
+
+    }
   }, [token])
 
   return [signer, setSigner]
 }
 
-export { useSigner, useToken };
+export { useSigner, useToken, useCheckSigner };
 export type { Token, Signer, SignerData };
