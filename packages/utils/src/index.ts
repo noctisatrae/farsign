6b8d4bbcc81cd351d706ff9d2fe9b9efa40dfc18
noctisatrae <pro.hector.kub@gmail.com>
2023-12-01 21:59:58 +0100

    bump ver & fix err

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -3,7 +3,18 @@ import { mnemonicToAccount } from "viem/accounts";
 import { bytesToHexString, hexStringToBytes } from "@farcaster/hub-web";
 import {Buffer} from "buffer";
 
-import * as contracts from "./contracts";
+const SIGNED_KEY_REQUEST_VALIDATOR_EIP_712_DOMAIN = {
+  name: "Farcaster SignedKeyRequestValidator",
+  version: "1",
+  chainId: 10,
+  verifyingContract: "0x00000000fc700472606ed4fa22623acf62c60553",
+} as const;
+
+const SIGNED_KEY_REQUEST_TYPE = [
+  { name: "requestFid", type: "uint256" },
+  { name: "key", type: "bytes" },
+  { name: "deadline", type: "uint256" },
+] as const;
 
 type keyGeneration = {
   publicKey: Uint8Array,
@@ -51,14 +62,14 @@ const generateSignedKeyRequestSignature = async (appFid: number, appMnemonic: st
   const deadline = Math.floor(Date.now() / 1000) + 86400;// signature is valid for 1 day
   
   return await account.signTypedData({
-    domain: contracts.SIGNED_KEY_REQUEST_VALIDATOR_EIP_712_DOMAIN,
+    domain: SIGNED_KEY_REQUEST_VALIDATOR_EIP_712_DOMAIN,
     types: {
-      SignedKeyRequest: contracts.SIGNED_KEY_REQUEST_TYPE,
+      SignedKeyRequest: SIGNED_KEY_REQUEST_TYPE,
     },
     primaryType: "SignedKeyRequest",
     message: {
       requestFid: BigInt(appFid),
-      // @ts-expect-error
+      /* @ts-expect-error */
       key,
       deadline: BigInt(deadline),
     },
